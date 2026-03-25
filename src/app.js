29e96ccee7fd4cb2add47c40bb3187e5ecdc1f41
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-03-25 10:44:58 +0200

    hw-js-frontend4-6-2

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import numbers, { sentence } from "./js/function";
+console.log(numbers(3, 6));
+
+
+
+
+import {centence} from "./js/function";
+console.log(sentence("добрий", " день"));
---
+++ src/js/function.js
@@ -0,0 +1,14 @@
+export default function numbers(a, b) {
+    if (a === b) {
+        return "числа одинакові";
+    }else{
+        return "числа не одинакові";
+    }
+}
+
+
+
+
+export function sentence(a, b) {
+    return a + b
+}
